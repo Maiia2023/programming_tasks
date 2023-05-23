150d14c4d0b2f69b1f9673b9c146e1aebfa992cf
Maya B <newmay4@gmail.com>
2023-05-23 21:22:41 +0300

    Implemented getCountOfVowels

---
+++ functions/getCountOfVowels/getCountOfVowels.js
@@ -0,0 +1,17 @@
+/**
+ * @function getCountOfVowels
+ * @param {string} str 
+ * @returns {number}
+ */
+
+export const getCountOfVowels = (str) => {
+  let count = 0;
+  const text = str.toLowerCase();
+  const vowels = 'aeiou';
+
+  for (const char of text) {
+    if (vowels.includes(char)) ++count;
+  }
+
+  return count;
+};
