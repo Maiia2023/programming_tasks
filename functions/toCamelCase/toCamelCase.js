2927e29bb4cbfd8a4cfdb90108b50e958c5d1f22
Maya B <newmay4@gmail.com>
2023-05-30 14:05:29 +0300

    Fixed

--- functions/toCamelCase/toCamelCase.js
+++ functions/toCamelCase/toCamelCase.js
@@ -1,12 +1,21 @@
+/**
+ * @function toCamelCase
+ * @param {string}
+ * @returns {string}
+ */
+
 export const toCamelCase = (str) => {
-  if (typeof str !== 'string') {
+  if (typeof str !== 'string')
     return '';
-  }
 
   const words = str.split(/[-_]/);
   const camelCaseText = words.map((word, index) => {
-    if (index === 0) return word;
-    return word.charAt(0).toUpperCase() + word.slice(1);
+    if (index === 0) {
+      const constant1 = word.charAt(0);
+      const constant2 = word.slice(1);
+      return `${constant1}${constant2}`;
+    }
+    return `${word.charAt(0).toUpperCase()}${word.slice(1)}`;
   });
 
   return camelCaseText.join('');
