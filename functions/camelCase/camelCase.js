7b4aae5fbe620d54921d362d760fc4dd318c1553
Maya B <newmay4@gmail.com>
2023-06-02 17:47:02 +0300

    Implement camelCase function

---
+++ functions/camelCase/camelCase.js
@@ -0,0 +1,22 @@
+/**
+ * @function camelCase
+ * @param {string}
+ * @returns {string}
+ */
+
+export const camelCase = (str) => {
+  if (typeof str !== 'string')
+    return '';
+
+  const words = str.split(/[-_]/);
+  const camelCaseText = words.map((word, index) => {
+    if (index === 0) {
+      const constant1 = word.charAt(0);
+      const constant2 = word.slice(1);
+      return `${constant1}${constant2}`;
+    }
+    return `${word.charAt(0).toUpperCase()}${word.slice(1)}`;
+  });
+
+  return camelCaseText.join('');
+};
