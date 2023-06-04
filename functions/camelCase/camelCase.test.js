fdeb644fd63331ebae0f0983af7c66e4cb7c56e9
Maya B <newmay4@gmail.com>
2023-06-04 19:33:54 +0300

    Fixed quotes in test file

--- functions/camelCase/camelCase.test.js
+++ functions/camelCase/camelCase.test.js
@@ -1,4 +1,4 @@
-import { camelCase } from "./camelCase.js";
+import { camelCase } from './camelCase.js';
 
 console.log(camelCase('the-stealth-warrior') === 'theStealthWarrior');
 console.log(camelCase('The_Stealth-Warrior') === 'TheStealthWarrior');
